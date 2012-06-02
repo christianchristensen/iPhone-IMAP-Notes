5e1ca989221142ad980c0c4cb3bc641972437ff9
Chris Christensen <christian.christensen@gmail.com>
2012-06-02 17:09:40 -0500

    Update app to index and begin web dev.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $notes = new Notes('mail.messagingengine.com', 'minenet@airpost.net', 'P@ssw0rd', 'INBOX');
 // TODO: Move these to tests
-//$debug = $notes->index();
+$debug = $notes->index();
 //$debug = $notes->create(rand(0, 1000) . "Hello There\n\n\n World One \n Two Three");
 //$debug = $notes->create("uh oh");
 //$debug = $notes->retrieve('4adb0460-acf5-11e1-81f0-7f6c2cc069f2');
 //$debug = $notes->retrieve(2);
-$debug = $debug = $notes->delete('410e98c0-acf5-11e1-a4c8-17f957629924');
+//$debug = $debug = $notes->delete('410e98c0-acf5-11e1-a4c8-17f957629924');
 print_r($debug);
