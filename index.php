66f9bc4f74374d9855d1efba60d82b4f84ee9b43
Chris Christensen <christian.christensen@gmail.com>
2012-06-03 08:49:13 -0500

    Add Oauth 1.0 callbacks and google URL's

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+define('CONS_KEY', 'deadbeef');
+define('CONS_SECRET', 'deadbeef');
 
 require_once __DIR__.'/vendor/autoload.php';
 
@@ -50,6 +52,83 @@ $app->match('/advauth', function(Symfony\Component\HttpFoundation\Request $reque
 
     return $app['twig']->render('advauth.twig', array());
 })->method('GET|POST');
+
+$app->get('/login', function () use ($app) {
+    $app['session']->start();
+    // check if the user is already logged-in
+    if (null !== ($username = $app['session']->get('user'))) {
+        return $app->redirect('/');
+    }
+
+    $oauth = new HTTP_OAuth_Consumer(CONS_KEY, CONS_SECRET);
+    $oauth->accept(new HTTP_Request2(NULL, NULL, array(
+            'ssl_cafile' => 'assets/mozilla.pem',
+    )));
+    $oauth->getRequestToken('https://www.google.com/accounts/OAuthGetRequestToken');
+    $oauth_token = $oauth->getToken();
+    $oauth_token_secret = $oauth->getTokenSecret();
+
+    $app['session']->set('access_token', $oauth_token);
+    $app['session']->set('access_secret', $oauth_token_secret);
+
+    return $app->redirect('https://www.google.com/accounts/OAuthAuthorizeToken?oauth_token=' . $oauth_token);
+});
+
+$app->get('/auth', function() use ($app) {
+    $app['session']->start();
+    // check if the user is already logged-in or we're already auth
+    if ((null !== $app['session']->get('username')) || (null !== $app['session']->get('auth_secret'))) {
+        return $app->redirect('/');
+    }
+
+    $oauth_token = $app['session']->get('access_token');
+    $secret = $app['session']->get('access_secret');
+    if ($oauth_token == null) {
+        $app->abort(400, 'Invalid token');
+    }
+
+    $oauth = new HTTP_OAuth_Consumer(CONS_KEY, CONS_SECRET);
+    $oauth->accept(new HTTP_Request2(NULL, NULL, array(
+            'ssl_cafile' => 'assets/mozilla.pem',
+    )));
+    $oauth->setToken($oauth_token);
+    $oauth->setTokenSecret($secret);
+    try {
+        $oauth->getAccessToken('https://www.google.com/accounts/OAuthGetAccessToken');
+    } catch (OAuthException $e) {
+        $app->abort(401, $e->getMessage());
+    }
+
+    // Set authorized token details for subsequent requests
+    $app['session']->set('auth_token', $oauth->getToken());
+    $app['session']->set('auth_secret', $oauth->getTokenSecret());
+
+    return $app->redirect('/req');
+});
+
+$app->get('/req', function () use ($app) {
+    $app['session']->start();
+    $token = $app['session']->get('auth_token');
+    // check if we have our auth keys
+    if (null === ($secret = $app['session']->get('auth_secret'))) {
+        return $app->redirect('/');
+    }
+    $oauth = new HTTP_OAuth_Consumer(CONS_KEY, CONS_SECRET);
+    $oauth->accept(new HTTP_Request2(NULL, NULL, array(
+            'ssl_cafile' => 'assets/mozilla.pem',
+    )));
+    $oauth->setToken($token);
+    $oauth->setTokenSecret($secret);
+    // TODO: Push this upstream to SDK lib
+    print_r('WE MADE IT!!');
+//     $oauth->sendRequest('https://www.allplayers.com/?q=api/v1/rest/groups/54395c18-f611-11e0-a44b-12313d04fc0f.json', array(), 'GET');
+//     $response = $oauth->getLastResponse();
+//     $json = json_decode($response->getResponse()->getBody());
+
+//     // HACK: set username to group UUID (eventually move this to users/current.json)
+//     $app['session']->set('username', $json->uuid);
+//     return $app->redirect('/');
+});
 
 function notesSessionMgr($app) {
     $app['session']->start();
