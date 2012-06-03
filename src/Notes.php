f4596d4a7fd9137e585930f77184f554af62aa06
Chris Christensen <christian.christensen@gmail.com>
2012-06-03 02:49:33 -0500

    Emulate iphone more accurately, remove message.

--- src/Notes.php
+++ src/Notes.php
@@ -110,6 +110,7 @@ class Notes
     public function delete($msgid = NULL) {
         $messageNum = $this->getMessageNum($msgid);
         $this->_storage->setFlags($messageNum, array(Storage::FLAG_DELETED, Storage::FLAG_SEEN));
+        $this->_storage->removeMessage($messageNum);
         return TRUE;
     }
 
